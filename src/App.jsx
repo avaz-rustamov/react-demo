6fa0e76d354ca06ac2a9b4105fe41b90870d33a6
avaz-rustamov <avaz.dev@icloud.com>
2022-02-16 23:17:25 +0500

    Add some text

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>AVAZ RUSTAMOV SENIOR FRONT-END REACT DEVELOPER</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Hello World!</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
