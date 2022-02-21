4270d6f9394b8bf4ed341e3d70516da1f290b76e
avaz-rustamov <avaz.dev@icloud.com>
2022-02-21 20:21:07 +0500

    Add redirects

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
       <header className="App-header">
-        <p>My first React App</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Hello World!</code>
+          <code>Hello World</code>
+          <h1>Samandar Abdulaxatov Epam Developer In Sha Alloh</h1>
         </p>
       </header>
     </div>
---
+++ src/components/header/header.jsx
@@ -0,0 +1,4 @@
+function Header() {
+  return <h1>Avaz...</h1>;
+}
+export default Header;
